442c5be9de20e27f4420c190d01a720fe9aff373
Burkhard Reffeling <burkhard.reffeling@gmail.com>
2019-08-21 21:28:36 +0200

    export new

--- packages/electron-redux/src/index.js
+++ packages/electron-redux/src/index.js
@@ -2,6 +2,7 @@ import forwardToMain, { forwardToMainWithParams } from './middleware/forwardToMa
 import forwardToRenderer from './middleware/forwardToRenderer';
 import triggerAlias from './middleware/triggerAlias';
 import createAliasedAction from './helpers/createAliasedAction';
+import expandAliasedAction from './helpers/expandAliasedAction';
 import replayActionMain from './helpers/replayActionMain';
 import replayActionRenderer from './helpers/replayActionRenderer';
 import getInitialStateRenderer from './helpers/getInitialStateRenderer';
@@ -12,6 +13,7 @@ export {
   forwardToRenderer,
   triggerAlias,
   createAliasedAction,
+  expandAliasedAction,
   replayActionMain,
   replayActionRenderer,
   getInitialStateRenderer,
